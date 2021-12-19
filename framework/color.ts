b18dfa1c96084ec2d729391acbe26b3c3fe9047a
Tyler Vezina <tylerjvezina@gmail.com>
2021-12-19 10:23:40 -0500

    2021 18 complete

--- framework/color.ts
+++ framework/color.ts
@@ -1,8 +1,8 @@
 function hexToRGB(hex: string): [number, number, number] {
   return [
-    parseInt(hex.substring(1, 3), 16),
-    parseInt(hex.substring(3, 5), 16),
-    parseInt(hex.substring(5, 7), 16),
+    parseInt(hex.slice(1, 3), 16),
+    parseInt(hex.slice(3, 5), 16),
+    parseInt(hex.slice(5, 7), 16),
   ];
 }
 

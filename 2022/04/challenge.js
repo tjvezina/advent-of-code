09d766bd7e740455e581f4787c27eda04dcaeec4
Tyler Vezina <tylerjvezina@gmail.com>
2022-12-04 00:36:04 -0500

    2022 4 complete

---
+++ 2022/04/challenge.js
@@ -0,0 +1,38 @@
+export const challenge = {
+  title: 'Camp Cleanup',
+
+  init() {
+    this.inputLines = this.input.split(/\r?\n/);
+  },
+
+  // --- Part 1 --- //
+  part1ExpectedAnswer: 456,
+  solvePart1() {
+    this.pairs = this.inputLines.map(line => {
+      const [minA, maxA, minB, maxB] = line.match(/(\d+)-(\d+),(\d+)-(\d+)/).slice(1, 5).map(x => parseInt(x));
+
+      return [
+        { min: minA, max: maxA },
+        { min: minB, max: maxB },
+      ];
+    });
+
+    const fullyOverlappingPairs = this.pairs.filter(pair => {
+      const [a, b] = pair;
+      return (a.min >= b.min && a.max <= b.max) || (a.min <= b.min && a.max >= b.max);
+    });
+
+    return ['There are {0} fully overlapping pairs.', fullyOverlappingPairs.length];
+  },
+
+  // --- Part 2 --- //
+  part2ExpectedAnswer: null,
+  solvePart2() {
+    const overlappingPairs = this.pairs.filter(pair => {
+      const [a, b] = pair;
+      return (a.min <= b.max && a.max >= b.min);
+    });
+
+    return ['There are {0} overlapping pairs.', overlappingPairs.length];
+  },
+}
